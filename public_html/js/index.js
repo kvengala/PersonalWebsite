6a051365658a095ed05788f165e25c4e0a3741fd
kvengala <kvengala@hawk.iit.edu>
2015-08-13 21:20:34 -0500

    final website

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -3,3 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+Parse.initialize("IDfYzMTWAIAzc5jmwiDJf9bNxrvxE7QZQjYC6Wdl", "qKGjumiYetRA8NjfyujrlIQMgjlL8cd3TEGOvJkh");
+
+var senddetail = function(){
+    var UserObject = Parse.Object.extend("User_information");
+    var text = document.getElementsByName("message")[0].value;
+    //alert(text);
+    if(text === "")
+    {
+        alert("Enter the text");
+        return false;
+    }
+    else{
+  var userObject = new UserObject();
+  userObject.save({Message: text}).then(function(object) {
+  alert(" Message has been sent");
+  document.getElementsByName("message")[0].value ="";
+});
+  return true;
+    }
+};
